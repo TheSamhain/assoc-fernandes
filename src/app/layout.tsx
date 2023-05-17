32c18e30a0383d58d15e08101caaff2a7f22e154
Leonardo Noro Pereira <leon.np99@gmail.com>
2023-05-16 21:54:31 -0300

    Ajuste nos estilo padrão

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import Header from '@/components/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import TabNavigation from '@/components/TabNavigation';
-import styles from './layout.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang='pt-BR'>
 			<body className={inter.className}>
 				<Header />
-				<main className={styles.main}>{children}</main>
+				<main style={{ flex: 1 }}>{children}</main>
 				<TabNavigation />
 			</body>
 		</html>
