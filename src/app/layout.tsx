e213f30d97c8c725b286c3d946d0838502282fb9
Leonardo Noro Pereira <leon.np99@gmail.com>
2023-06-07 13:46:32 -0300

    Incluído reset css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Header from '@/components/Header';
+import './reset.css';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import TabNavigation from '@/components/TabNavigation';
@@ -18,13 +19,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
 	return (
 		<html lang='pt-BR'>
-			<body className={inter.className}>
-				<ThemeProvider theme={darkTheme}>
+			<ThemeProvider theme={darkTheme}>
+				<body className={inter.className}>
 					<Header />
 					<main style={{ flex: 1, padding: '10px', overflow: 'auto' }}>{children}</main>
 					<TabNavigation />
-				</ThemeProvider>
-			</body>
+				</body>
+			</ThemeProvider>
 		</html>
 	);
 }
