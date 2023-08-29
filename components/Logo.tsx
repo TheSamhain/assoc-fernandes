c2fd50b0f2b9f3503da528356655e213c6f78d7b
Leonardo Noro Pereira <leon.np99@gmail.com>
2023-08-28 22:29:51 -0300

    Incluído titulo AFK no header

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,20 +1,23 @@
-import { Link } from 'expo-router';
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { Image, ImageStyle, StyleSheet, useColorScheme } from 'react-native';
+import { Image, ImageStyle, StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
 
+import { Text } from './Themed';
 import AssocLogoDark from '../assets/images/logo-karate-dark.png';
 import AssocLogoLight from '../assets/images/logo-karate.png';
 
 const Logo = () => {
+  const router = useRouter();
   const colorScheme = useColorScheme() ?? 'light';
   const imageSize = 50;
 
   const logoStyle: ImageStyle = { height: imageSize, width: imageSize };
 
   return (
-    <Link href='/' style={styles.logoContainer}>
+    <TouchableOpacity onPress={() => router.replace('/')} style={styles.logoContainer}>
+      <Text style={styles.title}>AFK</Text>
       <Image source={colorScheme === 'dark' ? AssocLogoDark : AssocLogoLight} style={logoStyle} />
-    </Link>
+    </TouchableOpacity>
   );
 };
 
@@ -23,5 +26,12 @@ export default Logo;
 const styles = StyleSheet.create({
   logoContainer: {
     padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    marginRight: 15,
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
