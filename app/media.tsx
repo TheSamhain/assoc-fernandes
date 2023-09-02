cc27da09c18522585864823d90e1e1ef1067226d
Leonardo Noro Pereira <leon.np99@gmail.com>
2023-09-02 12:47:57 -0300

    Incluído setas para trocar imagens

--- app/media.tsx
+++ app/media.tsx
@@ -1,11 +1,13 @@
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { Image } from 'expo-image';
-import { useLocalSearchParams, useNavigation } from 'expo-router';
-import React, { useEffect } from 'react';
+import { useLocalSearchParams, useNavigation, useRouter } from 'expo-router';
+import { listAll, ref, getDownloadURL } from 'firebase/storage';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 import { View } from '../components/Themed';
 import { blurHash } from '../constants/General';
+import { firebaseStorage } from '../utils/firebaseConfig';
 
 type ScreenMediaParams = {
   galery: string;
@@ -14,11 +16,46 @@ type ScreenMediaParams = {
 };
 
 const ScreenMedia = () => {
+  const router = useRouter();
   const navigation = useNavigation();
+
   const { galery, media, index } = useLocalSearchParams<ScreenMediaParams>();
+  const numIndex = Number(index);
   const decodedUrl = atob(media || '');
 
+  const [previousMedia, setPreviousMedia] = useState('');
+  const [nextMedia, setNextMedia] = useState('');
+
   useEffect(() => {
+    const getNeighborMedias = async () => {
+      setPreviousMedia('');
+      setNextMedia('');
+
+      if (!Number.isInteger(numIndex)) {
+        return;
+      }
+
+      const imagesRef = ref(firebaseStorage, galery);
+
+      const listRef = await listAll(imagesRef);
+      const { items } = listRef;
+
+      const previous = items[numIndex - 1];
+      const next = items[numIndex + 1];
+
+      if (previous) {
+        const url = await getDownloadURL(previous);
+        setPreviousMedia(url);
+      }
+
+      if (next) {
+        const url = await getDownloadURL(next);
+        setNextMedia(url);
+      }
+    };
+
+    getNeighborMedias();
+
     const title = (galery || 'Media').replace(/_|\//g, ' ').replace(/\d/g, '').trim();
     navigation.setOptions({ title });
   }, [media, index]);
@@ -26,8 +63,48 @@ const ScreenMedia = () => {
   return (
     <View style={styles.page}>
       <Image style={styles.image} source={decodedUrl} placeholder={blurHash} contentFit='contain' />
-      <MaterialCommunityIcons size={40} style={styles.lefIcon} color='#ffffff' name='chevron-left' />
-      <MaterialCommunityIcons size={40} style={styles.rightIcon} color='#ffffff' name='chevron-right' />
+
+      {previousMedia ? (
+        <MaterialCommunityIcons
+          size={40}
+          style={styles.lefIcon}
+          color='#ffffff'
+          name='chevron-left'
+          onPress={() =>
+            router.push({
+              pathname: 'media',
+              params: {
+                galery,
+                media: btoa(previousMedia || ''),
+                index: numIndex - 1,
+              },
+            })
+          }
+        />
+      ) : (
+        <></>
+      )}
+
+      {nextMedia ? (
+        <MaterialCommunityIcons
+          onPress={() =>
+            router.push({
+              pathname: 'media',
+              params: {
+                galery,
+                media: btoa(nextMedia || ''),
+                index: numIndex + 1,
+              },
+            })
+          }
+          size={40}
+          style={styles.rightIcon}
+          color='#ffffff'
+          name='chevron-right'
+        />
+      ) : (
+        <></>
+      )}
     </View>
   );
 };
