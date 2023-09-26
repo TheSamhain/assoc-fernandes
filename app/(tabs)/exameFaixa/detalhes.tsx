9662cbcdbef3b5f98c96291aff51231ede7171e0
Leonardo Noro Pereira <leon.np99@gmail.com>
2023-09-26 17:16:35 -0300

    Incluído loads na tela do exame

--- app/(tabs)/exameFaixa/detalhes.tsx
+++ app/(tabs)/exameFaixa/detalhes.tsx
@@ -1,12 +1,14 @@
 import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
-import React, { useEffect } from 'react';
+import { child, get, ref } from 'firebase/database';
+import React, { useEffect, useMemo, useState } from 'react';
 import { StyleSheet, ActivityIndicator, useColorScheme, ViewStyle, ScrollView } from 'react-native';
 
-import ExameData from '../../../assets/data/exame.json';
 import { Text, View } from '../../../components/Themed';
 import Colors from '../../../constants/Colors';
+import { ExameData } from '../../../interfaces/ExameData';
 import { getContrastingTextColor } from '../../../utils/Colors';
+import { firebaseDatabase } from '../../../utils/firebaseConfig';
 
 const ExameFaixaDetalhes = () => {
   const colorScheme = useColorScheme() ?? 'light';
@@ -22,35 +24,18 @@ const ExameFaixaDetalhes = () => {
     cor: string;
   }>();
 
-  const data = ExameData.sort((a, b) => b.kyu - a.kyu).reduce(
-    (acc, cur) => {
-      if (cur.kyu < Number(kyu)) {
-        return acc;
-      }
-
-      acc.bunkai.push(...cur.bunkai);
-      acc.geri.push(...cur.geri);
-      acc.kata.push(...cur.kata);
-      acc.tsuki.push(...cur.tsuki);
-      acc.uke.push(...cur.uke);
-
-      if (cur.kyu === Number(kyu)) {
-        acc.kyu = Number(kyu);
-      }
-
-      return acc;
-    },
-    {
-      bunkai: [],
-      geri: [],
-      kata: [],
-      kyu: 10,
-      tsuki: [],
-      uke: [],
-    },
-  );
+  const [exameData, setExameData] = useState<ExameData[]>([]);
 
   useEffect(() => {
+    const dbRef = ref(firebaseDatabase);
+    get(child(dbRef, 'exame')).then((snapshot) => {
+      if (snapshot.exists()) {
+        const exameDB = snapshot.val();
+
+        setExameData(exameDB);
+      }
+    });
+
     const title = faixa || 'Detalhes';
     navigation.setOptions({
       title,
@@ -60,6 +45,53 @@ const ExameFaixaDetalhes = () => {
       },
     });
   }, []);
+  const data = useMemo(
+    () =>
+      exameData
+        .sort((a, b) => b.kyu - a.kyu)
+        .reduce(
+          (acc, cur) => {
+            if (cur.kyu < Number(kyu)) {
+              return acc;
+            }
+
+            if (cur.bunkai) {
+              acc.bunkai.push(...cur.bunkai);
+            }
+
+            if (cur.geri) {
+              acc.geri.push(...cur.geri);
+            }
+
+            if (cur.kata) {
+              acc.kata.push(...cur.kata);
+            }
+
+            if (cur.tsuki) {
+              acc.tsuki.push(...cur.tsuki);
+            }
+
+            if (cur.uke) {
+              acc.uke.push(...cur.uke);
+            }
+
+            if (cur.kyu === Number(kyu)) {
+              acc.kyu = Number(kyu);
+            }
+
+            return acc;
+          },
+          {
+            bunkai: [],
+            geri: [],
+            kata: [],
+            kyu: 10,
+            tsuki: [],
+            uke: [],
+          },
+        ),
+    [exameData],
+  );
 
   if (!data) {
     return (
@@ -72,30 +104,44 @@ const ExameFaixaDetalhes = () => {
   return (
     <ScrollView style={[styles.page, pageStyle]}>
       <Text style={[styles.title, { marginTop: 0 }]}>Uke (Defesa)</Text>
-      {data.uke.map((item) => (
-        <Text style={styles.data}>
-          <Text style={styles.dataName}>{item.nome}</Text>: {item.descricao}
-        </Text>
-      ))}
+      {!data || !data.uke.length ? (
+        <ActivityIndicator size='large' color={Colors[colorScheme].text} />
+      ) : (
+        data.uke.map((item) => (
+          <Text style={styles.data}>
+            <Text style={styles.dataName}>{item.nome}</Text>: {item.descricao}
+          </Text>
+        ))
+      )}
 
       <Text style={styles.title}>Tsuki (Soco)</Text>
-      {data.tsuki.map((item) => (
-        <Text style={styles.data}>
-          <Text style={styles.dataName}>{item.nome}</Text>: {item.descricao}
-        </Text>
-      ))}
+      {!data || !data.tsuki.length ? (
+        <ActivityIndicator size='large' color={Colors[colorScheme].text} />
+      ) : (
+        data.tsuki.map((item) => (
+          <Text style={styles.data}>
+            <Text style={styles.dataName}>{item.nome}</Text>: {item.descricao}
+          </Text>
+        ))
+      )}
 
       <Text style={styles.title}>Geri (Chute)</Text>
-      {data.geri.map((item) => (
-        <Text style={styles.data}>
-          <Text style={styles.dataName}>{item.nome}</Text>: {item.descricao}
-        </Text>
-      ))}
+      {!data || !data.geri.length ? (
+        <ActivityIndicator size='large' color={Colors[colorScheme].text} />
+      ) : (
+        data.geri.map((item) => (
+          <Text style={styles.data}>
+            <Text style={styles.dataName}>{item.nome}</Text>: {item.descricao}
+          </Text>
+        ))
+      )}
 
       <Text style={styles.title}>Katas</Text>
-      {data.kata.map((item) => (
-        <Text style={styles.data}>{item}</Text>
-      ))}
+      {!data || !data.kata.length ? (
+        <ActivityIndicator size='large' color={Colors[colorScheme].text} />
+      ) : (
+        data.kata.map((item) => <Text style={styles.data}>{item}</Text>)
+      )}
 
       {data.bunkai && data.bunkai.length ? (
         <>
